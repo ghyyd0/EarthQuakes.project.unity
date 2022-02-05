2d52d1dabcfb52a916b3b2d92b4011acadd8b49f
alkihuri <alkihuri@gmail.com>
2022-02-05 23:14:23 +0200

    Almost Final Result

--- UnityProject/Assets/Scripts/EarthQuakeController.cs
+++ UnityProject/Assets/Scripts/EarthQuakeController.cs
@@ -7,16 +7,20 @@ public class EarthQuakeController : MonoBehaviour
     [SerializeField] AnimationCurve _earthQuakeGraph;
 
     [SerializeField, Range(0, 5)] float _earthQukaeTimeOn = 0;
+    [SerializeField, Range(0, 15)] float _magnitude = 1;
+    [SerializeField] GameObject _sphere;
 
     private void Start()
     {
+        _sphere = transform.GetChild(0).gameObject;
         _earthQukaeTimeOn = 0;
     }
     private void OnTriggerStay(Collider other)
     {
         _earthQukaeTimeOn = _earthQukaeTimeOn + Time.deltaTime / 15;
-        Vector3 directionOfForce = other.transform.position - transform.position;
-        directionOfForce = directionOfForce * _earthQuakeGraph.Evaluate(_earthQukaeTimeOn) * 10;
-        other.gameObject.GetComponent<Rigidbody>().AddRelativeForce(directionOfForce, ForceMode.Impulse);
+        var value = _earthQuakeGraph.Evaluate(_earthQukaeTimeOn);
+        var direction =   Vector3.one * value * Random.Range(-5,5)*  _magnitude;
+        other.gameObject.GetComponent<Rigidbody>().AddRelativeForce(direction,ForceMode.Impulse);
+        _sphere.transform.localScale = direction/ 50;
     }
 }
