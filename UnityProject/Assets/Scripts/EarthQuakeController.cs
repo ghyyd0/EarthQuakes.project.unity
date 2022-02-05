21e0106398ac8131d2d60509931fd6039d44339d
alkihuri <alkihuri@gmail.com>
2022-02-05 17:24:13 +0200

    First Test

---
+++ UnityProject/Assets/Scripts/EarthQuakeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthQuakeController : MonoBehaviour
+{
+    [SerializeField] AnimationCurve _earthQuakeGraph;
+
+    [SerializeField, Range(0, 5)] float _earthQukaeTimeOn = 0;
+
+    private void Start()
+    {
+        _earthQukaeTimeOn = 0;
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        _earthQukaeTimeOn = _earthQukaeTimeOn + Time.deltaTime / 15;
+        Vector3 directionOfForce = other.transform.position - transform.position;
+        directionOfForce = directionOfForce * _earthQuakeGraph.Evaluate(_earthQukaeTimeOn) * 10;
+        other.gameObject.GetComponent<Rigidbody>().AddRelativeForce(directionOfForce, ForceMode.Impulse);
+    }
+}
